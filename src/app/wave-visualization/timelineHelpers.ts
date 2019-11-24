60a9d65587b5fcd2d8ab9f0d348aa8475abc92a0
Holben888 <holben888@gmail.com>
2019-11-24 14:02:43 -0500

    fix slider, timeline, and spectrogram

---
+++ src/app/wave-visualization/timelineHelpers.ts
@@ -0,0 +1,66 @@
+import { TimelineModel } from './timeline-model'
+
+const getRandomColor = () =>
+  'rgba(' +
+  [
+    ~~(Math.random() * 255),
+    ~~(Math.random() * 255),
+    ~~(Math.random() * 255),
+    0.5,
+  ] +
+  ')'
+
+export const formatTimelineIntervals = pxPerSec => {
+  var retval = 1
+  if (pxPerSec >= 25 * 100) {
+    retval = 0.01
+  } else if (pxPerSec >= 25 * 40) {
+    retval = 0.025
+  } else if (pxPerSec >= 25 * 10) {
+    retval = 0.1
+  } else if (pxPerSec >= 25 * 4) {
+    retval = 0.25
+  } else if (pxPerSec >= 25) {
+    retval = 1
+  } else if (pxPerSec * 5 >= 25) {
+    retval = 5
+  } else if (pxPerSec * 15 >= 25) {
+    retval = 15
+  } else {
+    retval = Math.ceil(0.5 / pxPerSec) * 60
+  }
+  return retval
+}
+
+export const assignColorsToTimelineRegions = (
+  timeline: TimelineModel,
+  audioLength: number
+): TimelineModel => {
+  const colorMap = new Map()
+  timeline.regions.forEach(region => {
+    const assignedColor = colorMap.get(region.label)
+    if (!assignedColor) {
+      colorMap.set(region.label, getRandomColor())
+    }
+  })
+
+  return {
+    name: timeline.name,
+    regions: timeline.regions.map(region => {
+      const percentStart = (region.start / audioLength) * 100
+      const percentEnd = (region.end / audioLength) * 100
+      const percentWidth = percentEnd - percentStart
+
+      const cssStyles = {
+        width: percentWidth + '%',
+        left: percentStart + '%',
+        'background-color': colorMap.get(region.label),
+      }
+
+      return {
+        ...region,
+        cssStyles,
+      }
+    }),
+  }
+}
--- src/app/wave-visualization/wave-visualization.component.ts
+++ src/app/wave-visualization/wave-visualization.component.ts
@@ -4,50 +4,12 @@ import SpectrogramPlugin from 'wavesurfer.js/src/plugin/spectrogram'
 import RegionPlugin from 'wavesurfer.js/src/plugin/regions'
 import TimelinePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js'
 import { audioFile, audioLength, timelines } from '../../assets/dolphin-data.js'
-import { TimelineModel, RegionModel } from './timeline-model'
-import {ChangeContext, Options} from "ng5-slider";
-
-const getRandomColor = () =>
-  'rgba(' +
-  [
-    ~~(Math.random() * 255),
-    ~~(Math.random() * 255),
-    ~~(Math.random() * 255),
-    0.5,
-  ] +
-  ')'
-
-const assignColorsToTimelineRegions = (
-  timeline: TimelineModel
-): TimelineModel => {
-  const colorMap = new Map()
-  timeline.regions.forEach(region => {
-    const assignedColor = colorMap.get(region.label)
-    if (!assignedColor) {
-      colorMap.set(region.label, getRandomColor())
-    }
-  })
-
-  return {
-    name: timeline.name,
-    regions: timeline.regions.map(region => {
-      const percentStart = (region.start / audioLength) * 100
-      const percentEnd = (region.end / audioLength) * 100
-      const percentWidth = percentEnd - percentStart
-
-      const cssStyles = {
-        width: percentWidth + '%',
-        left: percentStart + '%',
-        'background-color': colorMap.get(region.label),
-      }
-
-      return {
-        ...region,
-        cssStyles,
-      }
-    }),
-  }
-}
+import { TimelineModel } from './timeline-model'
+import { ChangeContext } from 'ng5-slider'
+import {
+  assignColorsToTimelineRegions,
+  formatTimelineIntervals,
+} from './timelineHelpers'
 
 @Component({
   selector: 'app-wave-visualization',
@@ -65,33 +27,39 @@ export class WaveVisualizationComponent implements OnInit {
   multiplier = 1
 
   //ng-5 slider options
-  value: number = 5;
-  options: Options = {
-    floor: 0,
-    ceil: 20
-  };
+  ngSliderOptions = {
+    initialValue: 5,
+    range: {
+      floor: 0.1,
+      ceil: 5,
+    },
+  }
 
   constructor(public zone: NgZone) {
     this.waveformWidth = audioLength * this.zoomFactor * this.multiplier
   }
 
   ngOnInit() {
-
-    if(timelines.length > 0) {
+    if (timelines.length > 0) {
       this.timelines = []
     }
     timelines.forEach((timeline: TimelineModel) => {
-      const coloredTimeline = assignColorsToTimelineRegions(timeline)
+      const coloredTimeline = assignColorsToTimelineRegions(
+        timeline,
+        audioLength
+      )
       this.timelines.push(coloredTimeline)
     })
 
-    if(this.waveInstance != null) {
+    if (this.waveInstance != null) {
       this.waveInstance.destroy()
     }
+
     this.waveInstance = WaveSurfer.create({
       container: '#waveform',
       waveColor: '#ddd',
       progressColor: '#333',
+      normalize: true,
       plugins: [
         RegionPlugin.create(),
         SpectrogramPlugin.create({
@@ -100,23 +68,18 @@ export class WaveVisualizationComponent implements OnInit {
         }),
         TimelinePlugin.create({
           container: '#wave-timeline',
-          primaryLabelInterval: 1
-        })
-      ]
+          timeInterval: formatTimelineIntervals,
+        }),
+      ],
     })
 
-    // wave.on('ready', function () {
-    //   var timeline = Object.create(WaveSurfer.Timeline);
-    //    timeline.init({
-    //      wavesurfer: wave,
-    //      container: '#waveform-timeline'
-    //     });
-    // });
-    // wave.on('ready', function () {
-    //   console.log(WaveSurfer.Timeline);
-    //   var timeline = Object.create(WaveSurfer.Timeline);
-    //   console.log(timeline);
-    // });
+    this.waveInstance.on('ready', () => {
+      const spectrogramCanvas: HTMLElement = document.querySelector(
+        '#spectrogram > spectrogram > canvas:nth-of-type(2)'
+      )
+      spectrogramCanvas.style.minWidth = '100%'
+      // spectrogramCanvas.setAttribute('style', 'min-width: 100%')
+    })
 
     this.waveInstance.load(`../assets/${audioFile}`)
     this.waveInstance.zoom(this.zoomFactor * this.multiplier)
@@ -143,7 +106,9 @@ export class WaveVisualizationComponent implements OnInit {
     }
 
     // Update current playback time as audio plays
-    this.waveInstance.on('audioprocess', playbackTime => playbackHandler(playbackTime))
+    this.waveInstance.on('audioprocess', playbackTime =>
+      playbackHandler(playbackTime)
+    )
 
     // Update current playback time when user manually moves playhead
     this.waveInstance.on('seek', percentScrubbed =>
@@ -159,25 +124,14 @@ export class WaveVisualizationComponent implements OnInit {
     }
   }
 
-  onUserChangeStart($event: ChangeContext) {
-    console.log($event)
-    this.prevZoomFactor = $event.value;
-  }
-
   onUserChangeEnd($event: ChangeContext) {
-    const change = $event.value - this.prevZoomFactor;
-    if(change > 0) {
-      this.multiplier *= change;
-      console.log("CHECK start");
-      this.ngOnInit();
-      console.log("CHECK end");
-
-    } else if(change < 0){
-      this.multiplier /= change;
-      console.log("CHECK start");
-      this.ngOnInit();
-      console.log("CHECK end");
-    }
-
+    console.log($event.value)
+    this.waveInstance.spectrogram.width = 120 * $event.value * audioLength
+    console.log(this.waveInstance.spectrogram)
+    this.waveInstance.zoom(60 * $event.value)
+    this.zone.run(() => {
+      this.waveformWidth = 60 * $event.value * audioLength
+      console.log(this.waveformWidth)
+    })
   }
 }
